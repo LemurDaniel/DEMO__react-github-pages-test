e66c970fb9063aa4ce42fd8ffc4e0362cd6da8b1
LemurDaniel <Landau.Daniel.1998@gmail.com>
2023-05-01 04:50:05 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './css/Tailwind.css'
 import './css/App.css';
 
-import React, { useState, useEffect } from 'react';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Footer from './components/Footer';
+import Credits from './components/Credits';
 import Spacegame from './components/Spacegame';
 
 
@@ -14,12 +15,20 @@ function App() {
 
     <>
 
-
       <nav className="py-1 fixed w-full top-0 z-50      border-b border-brand2-100 bg-dark-700   flex justify-center">
         <button className="py-1 hover:underline text-brand2-100  font-bold focus:outline-none" > Space Game </button>
       </nav>
 
-      <Spacegame />
+
+      <Router basename="/DEMO__react-github-pages-test">
+
+        <Routes>
+          <Route path='/' exact element={<Spacegame />} />
+          <Route path='/credits' exact element={<Credits />} />
+        </Routes>
+
+
+      </Router>
 
       <Footer />
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,13 +13,14 @@ const Footer = () => {
         <footer className="pt-1 w-full   fixed bottom-0   flex flex-col md:flex-row justify-evenly   bg-dark-100 text-white text-xs font-bold">
 
             <div className="mx-auto mx:mx-0  pb-1">
-                <p className="px-2 inline "> <BiCopyright className="inline" /> <span className="font-extralight">copyright 2021 - </span> LemurDaniel</p>
+                <p className="px-2 inline "> <BiCopyright className="inline" /> <span className="font-extralight">copyright 2021 - </span> LemurDaniel </p>
                 <a href="https://github.com/LemurDaniel/PROJECT__React-app-1">  <IoLogoGithub   className="footer-icon" /> </a>
                 <a href="https://www.xing.com/profile/Daniel_Landau8/">         <IoLogoXing     className="footer-icon" /> </a>
                 <a href="https://www.linkedin.com/in/daniel-landau-ab2417188/"> <IoLogoLinkedin className="footer-icon" /> </a>
             </div>
 
             <div className="mx-auto mx:mx-0   py-1 md:py-0  border-gray-700 border-t md:border-t-0">
+                <a href="credits">  Credits </a>
                 <p className="px-2 inline">Technologies </p>
                 <a href="https://www.javascript.com/">  <IoLogoJavascript   className="footer-icon" /> </a>
                 <a href="https://reactjs.org/">         <IoLogoReact        className="footer-icon" /> </a>
